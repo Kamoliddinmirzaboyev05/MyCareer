c59fd61918e57e21be70296024a23f5d386edb83
Kamoliddinmirzaboyev05 <kamoliddinmirzaboyev2005@gmail.com>
2024-12-18 09:13:13 +0500

    Update

---
+++ ProfilDev/slider.js
@@ -0,0 +1,32 @@
+const leftBtn = document.querySelector(".left-btn"),
+  rightBtn = document.querySelector(".right-btn"),
+  slide = document.querySelector(".slide"),
+  slideImg = document.querySelector(".slide-img");
+
+images = [
+    '<img src="../project-img/apple.png" alt="">',
+  '<img src="../project-img/digital-agency.png" alt="">',
+  '<img src="../project-img/akademnashr.png" alt="">',
+  '<img src="../project-img/expedia.png" alt="expedia">',
+  '<img src="../project-img/figmaland.png" alt="">',
+  '<img src="../project-img/linkedtech.png" alt="">',
+];
+slideImg.innerHTML = '<img src="../project-img/apple.png" alt="">';
+
+var imageIndex = 0, activeImage;
+rightBtn.addEventListener("click", () => {
+   activeImage = imageIndex + 1;
+  if (activeImage < images.length) {
+    slideImg.innerHTML = images[activeImage];
+  }else{
+
+  }
+  imageIndex++;
+});
+leftBtn.addEventListener("click", () => {
+    activeImage = imageIndex - 1;
+   if (activeImage >= 0) {
+     slideImg.innerHTML = images[activeImage];
+   }
+   imageIndex--;
+ });
--- js/script.js
+++ js/script.js
@@ -5,30 +5,49 @@ document.addEventListener("DOMContentLoaded", () => {
     body = document.querySelector("body"),
     noVis = document.getElementById("noVis"),
     vis = document.getElementById("vis"),
-    pass = document.getElementById('pass');
+    pass = document.getElementById("pass"),
+    login = document.getElementById("login"),
+    loginInput = document.getElementById("loginInput"),
+    modalOyna = document.querySelector(".modal");
 
   loginBtn.addEventListener("click", () => {
-    modal.style.display = "block";
-    body.style.overflow = "hidden";
+    modal.style.top = "0";
+    modalOyna.style.top = "50%";
+    modalOyna.transition = "all 1s ease"
+    modalOyna.style.opacity = "1";
+    body.style.overflow = "auto";
   });
   close.addEventListener("click", () => {
-    modal.style.display = "none";
+    modal.style.top = "-1000px";
+    modalOyna.style.top = "-1000px";
     body.style.overflow = "auto";
   });
   window.addEventListener("click", (e) => {
     if (e.target == modal) {
       body.style.overflow = "auto";
-      modal.style.display = "none";
+      modalOyna.style.top = "-1000px";
+      modal.style.top = "-1000px";
     }
   });
   noVis.addEventListener("click", () => {
     noVis.style.display = "none";
-    vis.style.display = 'block'
-    pass.type = 'text'
+    vis.style.display = "block";
+    pass.type = "text";
   });
   vis.addEventListener("click", () => {
     noVis.style.display = "block";
-    vis.style.display = 'none'
-    pass.type = 'password'
+    vis.style.display = "none";
+    pass.type = "password";
+  });
+
+  login.addEventListener("click", () => {
+    if (pass.value == "" || loginInput.value == "") {
+      alert("Iltimos loginni va parolni kiriting!");
+    } else {
+      body.style.overflow = "auto";
+      modal.style.display = "none";
+      pass.value = "";
+      loginInput.value = "";
+    }
   });
 });
